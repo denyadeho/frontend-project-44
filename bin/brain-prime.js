f3676161133c14a5b88c475dc2397148cc146115
Denis <denyadeho@gmail.com>
2023-01-28 19:20:38 +0300

    added prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import askUser from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const name = askUser();
+console.log('Answer "yes" if the number is prime. Otherwise answer "no".');
+
+for (let i = 0; i < 3; i++) {
+  const randomNum = Math.floor(Math.random() * 101);
+  console.log(`Question: ${randomNum}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  const isPrime = (numb) => {
+    let count = 0;
+    for (let i = 1; i <= numb; i++) {
+      if (numb % i === 0) {
+        count += 1;
+      }
+    }
+    if (count === 2) {
+      return 'yes';
+    }
+    return 'no';
+  };
+
+  if (answer === isPrime(randomNum)) {
+    console.log('Correct!');
+  } else {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  if (i === 2) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
