6c6bfb04f1a6cbdb953277763195b2a12c01ac59
Denis <denyadeho@gmail.com>
2023-01-28 21:23:29 +0300

    refactor

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,16 +4,16 @@ import askUser from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = askUser();
-console.log('Answer "yes" if the number is prime. Otherwise answer "no".');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i += 1) {
   const randomNum = Math.floor(Math.random() * 101);
   console.log(`Question: ${randomNum}`);
   const answer = readlineSync.question('Your answer: ');
 
   const isPrime = (numb) => {
     let count = 0;
-    for (let j = 1; j <= numb; j++) {
+    for (let j = 1; j <= numb; j += 1) {
       if (numb % j === 0) {
         count += 1;
       }
@@ -23,7 +23,6 @@ for (let i = 0; i < 3; i++) {
     }
     return 'no';
   };
-
   if (answer === isPrime(randomNum)) {
     console.log('Correct!');
   } else {
