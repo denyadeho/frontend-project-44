00b1fb0f5fb4f14f6a451147544e9245cc09c1f8
Denis <denyadeho@gmail.com>
2023-02-09 00:07:33 +0300

    bug fix

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import askUser from '../src/cli.js';
 
@@ -8,20 +9,16 @@ for (let i = 0; i < 3; i += 1) {
   const randomNum = Math.floor(Math.random() * 101);
   console.log(`Question: ${randomNum}`);
   const answer = readlineSync.question('Your answer: ');
-
   if (randomNum % 2 === 0 && answer === 'yes') {
     console.log('Correct!');
   } else if (randomNum % 2 === 1 && answer === 'no') {
     console.log('Correct!');
-  }
-  else {
+  } else {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     console.log(`Let's try again, ${name}!`);
     break;
   }
-  }
-
   if (i === 2) {
     console.log(`Congratulations, ${name}!`);
   }
-
+}
